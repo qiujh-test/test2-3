827bdbc822dd62dde2d6da92b53eccad5cab093e
qiujh-test <qiujinhongzjhw@huawei.com>
2020-10-14 14:23:40 +0800

    步骤6

--- task03/instance.tf
+++ task03/instance.tf
@@ -2,7 +2,7 @@ resource "huaweicloud_compute_instance_v2" "ecs_example" {
     availability_zone = "cn-north-4a"
     name = "create_ecs"
     image_id = "01225754-3b48-47e4-8611-13663117b886"
-    flavor_name = "kc1.large.2"
+    flavor_name = var.flavor_name
 
     network {
     	uuid = huaweicloud_vpc_subnet_v1.subnet_v1.id
